7ff03eacc4f6dd55336a299d93a75cb32649ac34
Fred Ross <madhadron@gmail.com>
2011-05-19 17:13:54 +0200

    Finished code.  It works, and finds the centers put into it.
    Still need to write up a tutorial on what's going on so it can be used for the bein class.

--- threshold_pileup.py
+++ threshold_pileup.py
@@ -4,21 +4,21 @@ threshold_pileup.py
 by Fred Ross, <fred.ross@epfl.ch>
 May 19, 2011
 
-Takes a threshold and minimum region width and a CSV of
+Takes a threshold, a mean fragment size, and a CSV of
 chromosome,position,pileup and finds regions which are above the
-threshold.  Outputs them as a CSV of chromosome,start,one-past-end.
+threshold and sufficiently wide to be hits.  Then it uses the mean fragment size to estimate the position of the hit.  Outputs them as a CSV of chromosome,position.
 """
 import os
 import sys
 import csv
 import getopt
 
-usage = """Usage: threshold_pileup.py [-h] [-o output.txt] threshold min_width pileup.txt
+usage = """Usage: threshold_pileup.py [-h] [-o output.txt] threshold frag_len pileup.txt
 
 -h             Print this message and exit
 -o output.txt  Write output to output.txt (default: stdout)
 threshold      Positive integer specifying the threshold
-min_width      Minimum width of a region
+frag_len       Mean fragment size
 pileup.txt     CSV file of the form chromosome,position,pileup
 """
 
@@ -92,7 +92,7 @@ def main(argv=None):
         if len(args) != 3:
             raise Usage("threshold_pileup.py takes exactly three arguments.")
 
-        [threshold, min_width, pileup] = args
+        [threshold, frag_len, pileup] = args
 
         if not(os.path.exists(pileup)):
             raise Usage("Input file does not exist.")
@@ -103,26 +103,29 @@ def main(argv=None):
             raise Usage("Threshold must be a positive integer")
 
         try:
-            min_width = int(min_width)
+            frag_len = int(frag_len)
         except ValueError, v:
             raise Usage("Min_width must be a positive integer")
 
         if threshold < 1:
             raise Usage("Threshold must be a positive integer")
-        if min_width < 1:
+        if frag_len < 1:
             raise Usage("Threshold must be a positive integer")
 
         with open(pileup) as f:
-            r = csv.reader(pileup)
+            r = csv.reader(f)
             p = read_pileup(r)
+
+        min_width = (frag_len/2.0) * 0.7
 
         output_writer = csv.writer(output)
         keys = p.keys()
         keys.sort()
         for k in keys:
             p[k] = smooth(p[k])
-            [output_writer.writerow((k,start,end))
-             for start,end in find_regions(p[k], threshold)]
+            for start,end in find_regions(p[k], threshold, min_width):
+                pos = (end-start)/2.0 + start + frag_len/2.0
+                output_writer.writerow((k, pos))
         
         output.close()
         sys.exit(0)
