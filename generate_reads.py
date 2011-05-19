5fadc97516cad65fb8d515f1ec12217ef34d6329
Fred Ross <madhadron@gmail.com>
2011-05-19 11:44:44 +0200

    Finished sampler for generate_reads

--- generate_reads.py
+++ generate_reads.py
@@ -17,7 +17,9 @@ The reads are written as sequences, one per line, to the output.
 import os
 import sys
 import csv
+import math
 import getopt
+import random
 
 usage = """Usage: generate_reads.py [-h] [-s stdev] [-L frag_len] [-l read_len] [-o output] [-n n_reads] template.fasta centers.txt
 
@@ -58,7 +60,49 @@ def read_fasta(filename):
     sequences[current_id] = current_seq
     return sequences
 
+class Chooser(object):
+    def __init__(self, d):
+        self.z = []
+        i = 0
+        for k,v in d.iteritems():
+            i += v
+            self.z.append((k,i))
+        self.total = i
+    def sample(self):
+        q = random.randint(0, self.total-1)
+        for k,v in self.z:
+            if v >= q:
+                return k
 
+class Sampler(object):
+    def __init__(self, sequences, peaks, read_len, frag_len):
+        self.sequences = sequences
+        self.frag_len = frag_len
+        self.read_len = read_len
+        self.chromosome_chooser = Chooser(dict((n,len(s)) for n,s in sequences.iteritems()))
+        self.peak_bg_chooser = Chooser({'peak': sum(mag for chrom,pos,mag in peaks),
+                                        'bg': sum(len(s) for s in sequences.itervalues())})
+        self.peak_chooser = Chooser(dict(((chrom,pos),mag) for chrom,pos,mag in peaks))
+        
+    def sample(self):
+        if self.peak_bg_chooser.sample() == 'peak':
+            (chromosome,pos) = self.peak_chooser.sample()
+        else: # 'bg'
+            chromosome = self.chromosome_chooser.sample()
+            pos = random.randint(0, len(self.sequences[chromosome])-1)
+        # The offset from the position is geometrically
+        # distributed Let U be uniformly distributed on [0,1).
+        # Then ceil(ln(U) / ln(1-p)) is geometrically distributed
+        # with parameter p.  E[geometric] = 1/p, and we want the
+        # offset to the beginning of the fragment to be one half
+        # the fragment length, so p = 2/frag_len.
+        offset = max(0, int(math.ceil(math.log(random.random()) / math.log(1 - 2/float(self.frag_len)))))
+        read = self.sequences[chromosome][pos - offset : pos - offset + self.read_len]
+        if len(read) == self.read_len:
+            return read
+        else:
+            return self.sample()
+            
 def main(argv=None):
     output = sys.stdout
     read_len = 38
@@ -126,6 +170,7 @@ def main(argv=None):
         genome = read_fasta(fasta)
         with open(centers, 'r') as c:
             peaks = csv.reader(c)
+            peaks = [(chrom,int(pos),int(mag)) for chrom,pos,mag in peaks]
         
         sampler = Sampler(genome, peaks, read_len, frag_len, stdev)
         
